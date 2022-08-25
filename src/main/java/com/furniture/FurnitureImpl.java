6f15d1de87d0d8da5cc71199b4abb755f6ef126d
M V K <krishna.mugala9@gmail.com>
2022-08-25 22:57:26 +0530

    added java doc comments for the program

--- src/main/java/com/furniture/FurnitureImpl.java
+++ src/main/java/com/furniture/FurnitureImpl.java
@@ -9,8 +9,17 @@
 package com.furniture;
 
 public class FurnitureImpl {
+    // This is the main method which is the entry point of the program.
     public static void main(String[] args) {
         FurnitureItem furnitureItem = new FurnitureItem();
-        furnitureItem.s
+        furnitureItem.setCode("112232334");
+        furnitureItem.setColour("red");
+        furnitureItem.setGrade("grade1");
+        furnitureItem.setType("indor");
+        furnitureItem.setPrice(2500.5f);
+        float price = furnitureItem.getPrice();
+        System.out.println(" Amount After discount is : " + furnitureItem.calculateDiscount("12231423", "for me", "grade1", "outdoor", price));
+
+
     }
 }
--- src/main/java/com/furniture/FurnitureItem.java
+++ src/main/java/com/furniture/FurnitureItem.java
@@ -9,6 +9,7 @@
 package com.furniture;
 
 public class FurnitureItem {
+    static final int DISCOUNT = 5;
     private String code;
     private String type;
     private String grade;
@@ -16,59 +17,119 @@ public class FurnitureItem {
     private String usage;
     private float price;
 
-    static final int DISCOUNT = 5;
-
+    // A constructor.
     public FurnitureItem() {
     }
 
+    /**
+     * This function returns the code of the country
+     *
+     * @return The code of the course.
+     */
     public String getCode() {
         return code;
     }
 
+    /**
+     * This function sets the code of the object to the code passed in as a parameter
+     *
+     * @param code The code that you received from the user.
+     */
     public void setCode(String code) {
         this.code = code;
     }
 
+    /**
+     * This function returns the type of the current object
+     *
+     * @return The type of the object.
+     */
     public String getType() {
         return type;
     }
 
+    /**
+     * This function sets the type of the object to the type passed in as a parameter
+     *
+     * @param type The type of the event.
+     */
     public void setType(String type) {
         this.type = type;
     }
 
+    /**
+     * This function returns the grade of the student
+     *
+     * @return The grade of the student.
+     */
     public String getGrade() {
         return grade;
     }
 
+    /**
+     * This function sets the grade of the student
+     *
+     * @param grade The grade of the student.
+     */
     public void setGrade(String grade) {
         this.grade = grade;
     }
 
+    /**
+     * This function returns the colour of the car.
+     *
+     * @return The colour of the car.
+     */
     public String getColour() {
         return colour;
     }
 
+    /**
+     * This function sets the colour of the car to the colour passed in as a parameter.
+     *
+     * @param colour The colour of the car.
+     */
     public void setColour(String colour) {
         this.colour = colour;
     }
 
+    /**
+     * This function returns the usage of the command.
+     *
+     * @return The usage of the command.
+     */
     public String getUsage() {
         return usage;
     }
 
+    /**
+     * This function sets the usage of the command.
+     *
+     * @param usage The usage of the command.
+     */
     public void setUsage(String usage) {
         this.usage = usage;
     }
 
-    public double getPrice() {
+    /**
+     * This function returns the price of the item.
+     *
+     * @return The price of the item.
+     */
+    public float getPrice() {
         return price;
     }
 
+    /**
+     * This function sets the price of the item.
+     *
+     * @param price The price of the item.
+     */
     public void setPrice(float price) {
         this.price = price;
     }
 
+    // A method that calculates the discount on the price of the furniture item.
     public float calculateDiscount(String Code, String Type, String grade, String usage, float price) {
         if (grade == "grade1" && usage == "outdoor") {
             return this.price - this.price * DISCOUNT / 100;
