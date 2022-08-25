c8e2381da2d0804ebf28ce6239203a6471a56931
M V K <krishna.mugala9@gmail.com>
2022-08-25 22:15:47 +0530

    added getter and setter methods in the class furniture class

--- src/main/java/com/furniture/FurnitureItem.java
+++ src/main/java/com/furniture/FurnitureItem.java
@@ -16,5 +16,51 @@ public class FurnitureItem {
     private String usage;
     private double price;
 
+    public String getCode() {
+        return code;
+    }
 
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public String getUsage() {
+        return usage;
+    }
+
+    public void setUsage(String usage) {
+        this.usage = usage;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
